bb7447c64b6794f4c122cf2f614bdb1fea99db93
rusok <rusok.software@gmail.com>
2026-04-15 19:39:11 -0400

    Primer commit

---
+++ src/main/java/com/pepe/ejemmplokafka3/Ejemmplokafka3Application.java
@@ -0,0 +1,13 @@
+package com.pepe.ejemmplokafka3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemmplokafka3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemmplokafka3Application.class, args);
+	}
+
+}
